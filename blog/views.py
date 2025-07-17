c7fa0bf3372e1634af2ffa280af6b070a08c6bc2
Dillon-Mendoza <mudd.network@gmail.com>
2025-07-17 16:10:40 -0600

    Redirected logout to the login screen rather than the websites base.html

--- blog/views.py
+++ blog/views.py
@@ -4,6 +4,7 @@ from .models import Post
 from .forms import PostForm
 from django.shortcuts import redirect
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
 from django.contrib.auth import logout
 
 def post_list(request):
@@ -65,4 +66,25 @@ def post_remove(request, pk):
 
 def logout_view(request):
     logout(request)
-    return redirect('/')
+    return redirect('login')
+
+# def register_page(request):
+    if request.method == 'POST':
+        first_name = request.POST.get('first_name')
+        last_name = request.POST.get('last_name')
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        user = User.objects.filter(username=username)
+        if user.exists():
+            messages.info(request, "Username already taken!")
+            return redirect('/register/')
+        user = User.objects.create_user(
+            first_name=first_name,
+            last_name=last_name,
+            username=username
+        )
+        user.set_password(password)
+        user.save()
+        messages.info(request, "Account created Successfully!")
+        return redirect('/register/')
+    return render(request, 'register.html')
