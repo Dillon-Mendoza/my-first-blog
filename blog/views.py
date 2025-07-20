49606e7cc2b7254e32ad266933a561b185057ab2
Dillon-Mendoza <mudd.network@gmail.com>
2025-07-20 14:16:15 -0600

    Added the ability to make a comment, all things redirected properly and no errors rose when testing!

--- blog/views.py
+++ blog/views.py
@@ -7,6 +7,7 @@ from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
 from django.contrib.auth import logout
+from .forms import PostForm, CommentForm
 
 def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
@@ -68,3 +69,16 @@ def post_remove(request, pk):
 def logout_view(request):
     logout(request)
     return redirect('login')
+
+def add_comment_to_post(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    if request.method == "POST":
+        form = CommentForm(request.POST)
+        if form.is_valid():
+            comment = form.save(commit=False)
+            comment.post = post
+            comment.save()
+            return redirect('post_detail', pk=post.pk)
+    else:
+        form = CommentForm()
+    return render(request, 'blog/add_comment_to_post.html', {'form': form})
